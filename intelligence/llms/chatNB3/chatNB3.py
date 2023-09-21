2eb30b1f04cac86ede655b0490ec4a98472da6a1
kampff <adam.kampff@gmail.com>
2023-09-21 16:48:11 +0100

    Update TTS in chatNB3

--- intelligence/llms/chatNB3/chatNB3.py
+++ intelligence/llms/chatNB3/chatNB3.py
@@ -2,7 +2,6 @@ import os
 import sys
 import time
 import openai
-import pyttsx3
 import numpy as np
 import curses
 
@@ -23,36 +22,32 @@ input_wav_path = box_path + '/_tmp/input.wav'
 output_wav_path = box_path + '/_tmp/output.wav'
 
 # Set OpenAI API Key (secret!!!)
-openai.api_key = "<secret-message>"
+openai.api_key = "<secret-key>"
 
 # Initialize conversation history
 conversation = [
     {"role": "system", "content": "You are small two wheeled robot shaped like a brain. Your name is NB3, which stands for no black box bot. Your task is to respond to questions about neuroscience and technology, or anything really, with a short snarky but accurate reply."},
 ]
 
-# Initialize speech engine
-engine = pyttsx3.init()
-
 # Specify params
-input_device = 1
-output_device = 1
+input_device = 0
+output_device = 0
 num_input_channels = 2
 num_output_channels = 1
-input_sample_rate = 44100
-output_sample_rate = 22050
-input_buffer_size = int(input_sample_rate / 100)
-output_buffer_size = int(output_sample_rate / 100)
-max_samples = int(output_sample_rate * 10)
+sample_rate = 16000
+buffer_size = int(sample_rate / 100)
+buffer_size = int(sample_rate / 100)
+max_samples = int(sample_rate * 10)
 
 # List available sound devices
 sound.list_devices()
 
 # Initialize microphone
-microphone = sound.microphone(input_device, num_input_channels, 'int16', input_sample_rate, input_buffer_size, max_samples)
+microphone = sound.microphone(input_device, num_input_channels, 'int16', sample_rate, buffer_size, max_samples)
 microphone.start()
 
 # Initialize speaker
-speaker = sound.speaker(output_device, num_output_channels,  'int16', output_sample_rate, output_buffer_size)
+speaker = sound.speaker(output_device, num_output_channels,  'int16', sample_rate, buffer_size)
 speaker.start()
 
 # Clear error ALSA/JACK messages from terminal
@@ -89,8 +84,8 @@ try:
                 sys.exit()
             elif char == ord('z'):
                 break
-        microphone.save_wav(input_wav_path, max_samples)
         screen.erase()        
+        microphone.save_wav(input_wav_path, max_samples)
 
         # Get transcription from Whisper
         audio_file= open(input_wav_path, "rb")
@@ -111,8 +106,8 @@ try:
         conversation.append({'role': 'assistant', 'content': f'{reply}'})
 
         # Speak reply
-        engine.save_to_file(reply, output_wav_path)
-        engine.runAndWait()
+        cmd = "echo \"{0}\" | piper --model en_US-lessac-low.onnx --output_file {1}".format(reply, output_wav_path)
+        result = os.system(cmd)
         speaker.play_wav(output_wav_path)
         screen.addstr(8, 0, "NB3: {0}\n".format(reply), curses.A_NORMAL)
         while speaker.is_playing():
