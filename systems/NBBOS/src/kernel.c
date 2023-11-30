507b4af8ffb099c90344525181ede5fca65dae01
kampff <adam.kampff@gmail.com>
2023-11-30 14:13:19 +0000

    CLeanup NBBOS drafts (makefile)

--- systems/NBBOS/src/kernel.c
+++ systems/NBBOS/src/kernel.c
@@ -1,21 +1,28 @@
 #include "io.h"
 #include "fb.h"
+#include "gpio.h"
+
+/** GPIO Register set */
+volatile unsigned int* gpio = (unsigned int*)GPIO_BASE;
 
 void main()
 {
     uart_init();
     fb_init();
+    gpio[LED_GPFSEL] |= (1 << LED_GPFBIT); // Set as output
 
     int rg = 0;
     while (1)
     {
         if(rg == 1)
         {
+            gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
             fillBackground(0x04);
             rg = 0;
         }
         else
         {
+            gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
             fillBackground(0x02);
             rg = 1;
         }
