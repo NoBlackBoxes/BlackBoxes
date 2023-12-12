e73f75a628f9d29c68d74d19b9a0cbd707c5486d
kampff <adam.kampff@gmail.com>
2023-12-12 14:48:12 +0000

    Cache enabled (no MMU): 80 FPS

--- systems/NBBOS/level_0/include/common.h
+++ systems/NBBOS/level_0/include/common.h
@@ -11,3 +11,4 @@ extern void PUT32 (uint64_t address, uint32_t value);
 extern uint32_t GET32 (uint64_t address);
 extern void TICK (uint64_t count);
 extern uint32_t GETEL (void);
+extern void __memset_aarch64(uint32_t *dst, uint32_t val, uint32_t count);
